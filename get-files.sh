2dc3679b669d1c67b5076e4a3eb35c004b3ae5bd
Yuren Ju <yurenju@gmail.com>
2011-07-22 17:53:46 -0700

    change folder from /images to static/images

--- get-files.sh
+++ get-files.sh
@@ -12,16 +12,16 @@ avatars[7]="7fda1da9c34e978d5990afd7f58ca0f4"
 avatars[8]="ca021af91df7c161d5586fdd6a46834d"
 avatars[9]="c63392ca320086522cf4d55cbf1d3808"
 
-mkdir -p images/original images/thumbnails images/avatars
+mkdir -p static/images/original static/images/thumbnails static/images/avatars
 
 for i in {1480..1450}
 do 
-    wget http://220.133.12.74/Share/Large/$i.jpeg -O images/original/$i.jpeg
-    wget http://220.133.12.74/Share/Small/$i.jpeg -O images/thumbnails/$i.jpeg
+    wget http://220.133.12.74/Share/Large/$i.jpeg -O static/images/original/$i.jpeg
+    wget http://220.133.12.74/Share/Small/$i.jpeg -O static/images/thumbnails/$i.jpeg
 done
 
 for i in {0..9}
 do
     wget "http://0.gravatar.com/avatar/${avatars[$i]}?s=69&d=monsterid" \
-         -O images/avatars/${avatars[$i]}.jpg
+         -O static/images/avatars/${avatars[$i]}.jpg
 done
